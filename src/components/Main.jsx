4bcf8530cce9eea77060fdd4578b6c7bba4a6682
aktech27 <ahamedkbr85@gmail.com>
2021-11-11 12:14:01 +0530

    Added Animation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,9 @@ const Main = () => {
   return (
     <main className="main-container">
       <div>
-        <h1 id="company-name">Victory Industries</h1>
+        <h1 id="company-name" className="appear">
+          Victory Industries
+        </h1>
         <h2 id="company-gst" className="typewrite">
           33AHXPR7557F1ZM
         </h2>
