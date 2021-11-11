0aa6f3f0ebc4554671336671b4f82e04af11d517
aktech27 <ahamedkbr85@gmail.com>
2021-11-11 11:41:51 +0530

    Initial Deploy Commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import "./Main.css";
+const Main = () => {
+  return (
+    <main className="main-container">
+      <div>
+        <h1 id="company-name">Victory Industries</h1>
+        <h2 id="company-gst" className="typewrite">
+          33AHXPR7557F1ZM
+        </h2>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./Navbar.css";
+import Logo from "../assets/logo.png";
+const Navbar = () => {
+  return (
+    <nav className="top-nav" id="navbar">
+      <a href="#home">
+        <img src={Logo} alt="home" />
+      </a>
+      <a href="#products">Products</a>
+      <a href="#about">About</a>
+      <a href="#contact">Contact</a>
+      <span
+        className="icon"
+        onClick={() => document.querySelector("#navbar").classList.toggle("responsive")}
+      >
+        <i className="fa fa-bars"></i>
+      </span>
+    </nav>
+  );
+};
+
+export default Navbar;
