baf7d2650cff158dc67cc014fea1b909fc3e08de
aktech27 <ahamedkbr85@gmail.com>
2021-11-16 11:01:20 +0530

    Added About section

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import Logo from "../assets/owner.jpg";
+import "./About.css";
+const About = () => {
+  return (
+    <section id="about">
+      <div className="info-container">
+        Victory Industries - established in the year 2017, we are leading manufacturer in Electrical
+        and Panel Board Accessories.Our products are shipped to the customers based on their
+        requirments and satisfaction .Our main focus is to provide high quality products to our
+        customers.
+      </div>
+      <div className="dp-container">
+        <img src={Logo} alt="DP" />
+      </div>
+    </section>
+  );
+};
+
+export default About;
