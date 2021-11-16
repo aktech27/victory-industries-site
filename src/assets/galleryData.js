0f7de7e5c64ccda3f47e26334238c358dcbeb45a
aktech27 <ahamedkbr85@gmail.com>
2021-11-16 18:59:15 +0530

    Client Changes

--- src/assets/galleryData.js
+++ src/assets/galleryData.js
@@ -1,30 +1,34 @@
 export const imagesArray = [
   {
-    name: "Caption 1",
+    name: "COMPANY NAME",
     Url: "https://drive.google.com/uc?export=view&id=10gvJo7qwEHrerArlIj7xsB4nbpHiCVh1",
   },
   {
-    name: "Caption 2",
+    name: "MCB CHANNEL SLOTTED - YELLOW",
     Url: "https://drive.google.com/uc?export=view&id=1wOzskXRZWkA3eYMOqy16HPcOvW6N2yNQ",
   },
   {
-    name: "Caption 3",
+    name: "MCB CHANNEL SLOTTED - WHITE",
     Url: "https://drive.google.com/uc?export=view&id=1-rBEcfdVJAQzcXg23B8dN-RbDVQvglQ5",
   },
   {
-    name: "Caption 4",
+    name: "DYE COLORS",
     Url: "https://drive.google.com/uc?export=view&id=17cqlV8PgCQ-koKYP1fagiDaVySxoCi7K",
   },
   {
-    name: "Caption 5",
+    name: "MCB PLAIN - UNPLATTED",
     Url: " https://drive.google.com/uc?export=view&id=1D3qYmQbUwPTnq0_R0z5vV-jt2cDKfbjQ",
   },
   {
-    name: "Caption 6",
+    name: "MCB vs TB",
     Url: "https://drive.google.com/uc?export=view&id=1xub2lXbuqtLUbyJT-O8PF7OIbbo71tkr",
   },
 ];
 export const videosArray = [
-  { Url: "https://www.youtube.com/embed/5IZEk8E3S0A" },
+  { Url: "https://www.youtube.com/embed/9DKcKwp6w9U" },
+  { Url: "https://www.youtube.com/embed/yHpYROgW5Uw" },
+  { Url: "https://www.youtube.com/embed/CNBLfYtaGyw" },
+  { Url: "https://www.youtube.com/embed/JZPnAaCUxBI" },
+  { Url: "https://www.youtube.com/embed/DoEtvLCdWdE" },
   { Url: "https://www.youtube.com/embed/5IZEk8E3S0A" },
 ];
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section id="about">
       <div className="info-container">
-        Victory Industries - established in the year 2017, we are leading manufacturer in Electrical
+        Victory Industries - established in the year 2000, we are leading manufacturer in Electrical
         and Panel Board Accessories.Our products are shipped to the customers based on their
         requirments and satisfaction .Our main focus is to provide high quality products to our
         customers.
